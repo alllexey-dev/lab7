10b9f2f9a1a54cef389929aa80ed5e0bd9e28d7b
3fant <3fantimofey@gmail.com>
2026-05-11 15:24:48 +0300

    refactor: env to properties rename

--- server/src/main/kotlin/ServerContainer.kt
+++ server/src/main/kotlin/ServerContainer.kt
@@ -2,7 +2,7 @@ import application.CommandInvoker
 import data.StorageManager
 import org.apache.logging.log4j.Level
 import org.apache.logging.log4j.kotlin.logger
-import util.EnvParser
+import util.PropertiesParser
 import java.net.InetSocketAddress
 import java.nio.channels.SelectionKey
 import java.nio.channels.Selector
@@ -21,7 +21,7 @@ class ServerContainer(
     var hostname = ""
 
     init {
-        val env = EnvParser.getEnvFromFile(".env")
+        val env = PropertiesParser.getPropertiesFromFile(".env")
         serverPort = env["SERVER_PORT"] ?: throw Error("server port should be specified in env")
         hostname = env["HOST_NAME"] ?: throw Error("hostname should be specified in env")
     }
@@ -38,7 +38,7 @@ class ServerContainer(
         serverSocket.configureBlocking(false)
         serverSocket.register(selector, SelectionKey.OP_ACCEPT)
 
-        println("Сервер запущен на 127.0.0.1:$serverPort")
+        println("Server started at 127.0.0.1:$serverPort")
         while (true) {
             val input = IO.processInput()
             if (input != null) {
