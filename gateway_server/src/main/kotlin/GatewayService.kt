2ecc125dee9546c262af9cbee2ec681d4e61809c
3fant <3fantimofey@gmail.com>
2026-05-15 14:26:22 +0300

    feat: gateway + balancer add

---
+++ gateway_server/src/main/kotlin/GatewayService.kt
@@ -0,0 +1,88 @@
+import org.apache.logging.log4j.kotlin.logger
+import util.PropertiesParser
+import java.net.InetSocketAddress
+import java.nio.channels.SelectionKey
+import java.nio.channels.Selector
+import java.nio.channels.ServerSocketChannel
+
+class GatewayService {
+    val logger = logger()
+    var balancer = Balancer()
+    var serverPort = ""
+    var hostname = ""
+
+    init {
+        val env = PropertiesParser.getPropertiesFromFile(".env")
+        serverPort = env["SERVER_PORT"] ?: throw Error("server port should be specified in env")
+        hostname = env["HOST_NAME"] ?: throw Error("hostname should be specified in env")
+    }
+
+    fun up() {
+        try {
+            val selector = Selector.open()
+            val serverSocket = ServerSocketChannel.open()
+            serverSocket.bind(
+                InetSocketAddress(
+                    hostname,
+                    serverPort.toIntOrNull() ?: throw Error("check for server port format in env file")
+                )
+            )
+            serverSocket.configureBlocking(false)
+            serverSocket.register(selector, SelectionKey.OP_ACCEPT)
+
+            println("Gateway started at 127.0.0.1:$serverPort")
+            while (true) {
+                selector.selectNow()
+                val selectionIterator = selector.selectedKeys().iterator()
+                while (selectionIterator.hasNext()) {
+                    val key = selectionIterator.next()
+                    logger.info { key.toString() }
+                    selectionIterator.remove()
+
+                    if (!key.isValid) continue
+
+                    if (key.isAcceptable) {
+                        val client = serverSocket.accept()
+                        logger.info { client.toString() }
+                        client.configureBlocking(false)
+
+                        val io = ClientsToGatewayChannel(client)
+                        client.register(selector, SelectionKey.OP_READ, io)
+
+                        println("Client connected: ${client.remoteAddress}")
+                    }
+
+                    if (key.isReadable) {
+
+                        val io = key.attachment() as ClientsToGatewayChannel
+
+                        try {
+
+                            val request = io.read()
+                            logger.info { request.toString() }
+                            if (request != null) {
+                                println("Получен запрос: $request")
+                                val response = balancer.handle(request)
+                                logger.info { response }
+                                try {
+                                    io.write(response)
+                                } catch (e: Exception) {
+                                    logger.warn { e.message ?: "" }
+                                    e.printStackTrace()
+                                }
+                            }
+                        } catch (e: Exception) {
+                            logger.info { e.message }
+                            println("Клиент отключился или произошла ошибка")
+                            key.channel().close()
+                            key.cancel()
+                        }
+                    }
+                }
+            }
+        } catch (_: ExitSignal) {
+            println("Сервер выключается.")
+            return
+        }
+    }
+}
---
+++ gateway_server/src/main/kotlin/GatewayToServersChannel.kt
@@ -0,0 +1,54 @@
+import kotlinx.serialization.encodeToString
+import kotlinx.serialization.json.Json
+import java.io.IOException
+import java.nio.ByteBuffer
+import java.nio.channels.SocketChannel
+
+class GatewayToServersChannel(
+    private val channel: SocketChannel,
+) {
+    private var size = -1
+    private val sizeBuffer = ByteBuffer.allocate(4)
+    private lateinit var dataBuffer: ByteBuffer
+
+    fun read(): Response? {
+        if (size == -1) {
+            val bytesRead = channel.read(sizeBuffer)
+            if (bytesRead == -1) throw IOException("Соединение с сервером разорвано")
+            if (sizeBuffer.hasRemaining()) return null
+
+            sizeBuffer.flip()
+            size = sizeBuffer.int
+            sizeBuffer.clear()
+
+            dataBuffer = ByteBuffer.allocate(size)
+        }
+
+        val bytesReadData = channel.read(dataBuffer)
+        if (bytesReadData == -1) throw IOException("Соединение с сервером разорвано")
+
+        if (dataBuffer.hasRemaining()) return null
+
+        val json = String(dataBuffer.array(), Charsets.UTF_8)
+        val rpc = Json.decodeFromString<Response>(json)
+
+        size = -1
+
+        return rpc
+    }
+
+    fun write(message: Request) {
+        val json = Json.encodeToString(message)
+        val bodyBytes = json.toByteArray(Charsets.UTF_8)
+
+        val writeBuffer = ByteBuffer.allocate(4 + bodyBytes.size)
+        writeBuffer.putInt(bodyBytes.size)
+        writeBuffer.put(bodyBytes)
+        writeBuffer.flip()
+
+        while (writeBuffer.hasRemaining()) {
+            val written = channel.write(writeBuffer)
+            if (written == -1) throw Exception("Disconnected while writing")
+        }
+    }
+}
