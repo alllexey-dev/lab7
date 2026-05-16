e2b4116c9dc4c55b46a7bf400d9d298ecdd6d27b
3fant <3fantimofey@gmail.com>
2026-05-16 14:08:31 +0300

    fix: id problem fix + some minor mistakes (e.g. executeQuery to executeUpdate)

--- server/src/main/kotlin/domain/OrganizationRepository.kt
+++ server/src/main/kotlin/domain/OrganizationRepository.kt
@@ -1,7 +1,7 @@
 package domain
 
 interface OrganizationRepository {
-    fun add(organization: Organization)
+    fun add(organization: Organization, id: Int)
     fun updateById(id: Int, organization: Organization)
     fun removeById(id: Int)
 }
